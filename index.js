7feaeed20479b038c978ff397369ba0444e09691
galihanggara68 <galihanggara68@gmail.com>
2021-06-25 13:47:06 +0700

    + Added server for hit

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const { Client } = require('whatsapp-web.js');
 const fs = require("fs");
 const SESSION_FILE_PATH = "./session.json";
 
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/sendMessage", async (req, res, next) => {
+	const {message, number} = req.body;
+	const contact = await client.getContactById(number);
+	let chat = await contact.getChat();
+	await chat.sendMessage(message);
+	res.send({message, number});
+});
+
 let sessionData;
 if(fs.existsSync(SESSION_FILE_PATH)) {
     sessionData = require(SESSION_FILE_PATH);
@@ -16,8 +29,10 @@ client.on('qr', (qr) => {
     qrcode.generate(qr, {small: true});
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on('ready', async () => {
+    app.listen(8080, () => {
+		console.log("Server up");
+	});
 });
 
 client.on('authenticated', (session) => {    
